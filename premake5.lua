de0c101ec0085bdf9ba3d4841d0fba2c93cfb09c
석원 박 <rty3394@naver.com>
2025-04-16 16:04:53 +0900

    sync fork, add premake file

---
+++ premake5.lua
@@ -0,0 +1,45 @@
+project "ImGui"
+	kind "StaticLib"
+	language "C++"
+    cppdialect "C++20"
+    staticruntime "on"
+
+	targetdir ("Build/Bin/%{cfg.buildcfg}/%{prj.name}")
+	objdir ("Build/Intermediate/%{cfg.buildcfg}/%{prj.name}")
+
+	files
+	{
+		"imconfig.h",
+		"imgui.h",
+		"imgui.cpp",
+		"imgui_draw.cpp",
+		"imgui_internal.h",
+		"imgui_widgets.cpp",
+        "imgui_tables.cpp",
+		"imstb_rectpack.h",
+		"imstb_textedit.h",
+		"imstb_truetype.h",
+		"imgui_demo.cpp"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++20"
+		staticruntime "On"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+    filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
+        symbols "off"
